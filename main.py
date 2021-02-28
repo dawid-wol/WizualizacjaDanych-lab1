0de1669bf0acaf8c38d510d6564d0d5a8d020300
dawid-wol <bigcoconut01@gmail.com>
2021-02-28 18:18:17 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-#zad1
-a = 8
-print(a)
-a = 8 * 2
-print(a)
+#Zad1
+calkowita1 = 12
+calkowita2 = 4
+rzeczywista1 = 4.5
+rzeczywista2 = float(48)
+napis1 = "Latajacy cyrk Monthy Pythona"
+napis2 = 'Witaj swiecie!'
+print(calkowita1)
+print(calkowita2)
+print(rzeczywista1)
+print(rzeczywista2)
+print(napis1)
+print(napis2)
+#Zad2
