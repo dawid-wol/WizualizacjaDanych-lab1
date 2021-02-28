ee82d377d72d0ba3b237e80dccc6558f96f34e50
dawid-wol <bigcoconut01@gmail.com>
2021-02-28 18:43:37 +0100

    Initial commit

--- main.py
+++ main.py
@@ -11,4 +11,22 @@ print(rzeczywista1)
 print(rzeczywista2)
 print(napis1)
 print(napis2)
+
 #Zad2
+liczba1 = input("Podaj Liczbe: ")
+liczba2 = input("Podaj Liczbe: ")
+wynik1 = int(liczba1) + int(liczba2)
+wynik2 = int(liczba1) - int(liczba2)
+wynik3 = int(liczba1) * int(liczba2)
+wynik4 = int(liczba1) / int(liczba2)
+print("Dodawanie: ")
+print(wynik1)
+print("Odejmowanie: ")
+print(wynik2)
+print("Mnożenie: ")
+print(wynik3)
+print("Dzielenie: ")
+print(wynik4)
+
+#Zad3
+
